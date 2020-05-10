46ce912f50e5ec09d5448118afbe2c611dcf4743
zhanquanle <zhanquanle@gmail.com>
2020-05-10 10:21:08 +0800

    zdy_rpc

--- rpc_consumer/src/main/java/com/quanle/client/RpcClientBootStrap.java
+++ rpc_consumer/src/main/java/com/quanle/client/RpcClientBootStrap.java
@@ -11,18 +11,6 @@ import java.util.Collections;
  * @date 2020/4/28 9:25 PM
  */
 public class RpcClientBootStrap {
-//    public static final String providerName = "UserService#sayHello#";
-
-//    public static void main(String[] args) throws InterruptedException {
-//        RpcConsumer rpcConsumer = new RpcConsumer();
-//        UserService proxy = (UserService) rpcConsumer.createProxy(UserService.class);
-//        while (true) {
-//            System.out.println("============>>>>>");
-//            Thread.sleep(2000);
-//            proxy.sayHello("are you ok?");
-//            System.out.println("已响应");
-//        }
-//    }
 
     public static void main(String[] args) throws Exception {
         String connectionString = "192.168.3.3:2181";
